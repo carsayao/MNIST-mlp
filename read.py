Tracking sigma overflow bug

read.py
@@ -36,18 +36,21 @@ def read_to_dat():
                 images_path = test_images_raw,
                 labels_path = test_labels_raw)
         return x, y
-    def convert_targets_train(array):
+    def convert_targets_train(train_array):
         # target_array = np.zeros((SAMPLES, NEURONS), dtype=float)
-        target_array = np.empty((SAMPLES, NEURONS), dtype='float64')
+        target_array = np.empty((SAMPLES, NEURONS), dtype='float32')
         target_array.fill(0.1)
         for t in range(SAMPLES):
-            target_array[t][int(array[t])] = .9
-        print(target_array[0])
+            target_array[t][int(train_array[t])] = 0.9
+        print(target_array[59999])
         return target_array
-    def convert_targets_test(array):
-        target_array = np.zeros((SAMPLES_T, NEURONS), dtype=int)
+    def convert_targets_test(test_array):
+        # target_array = np.zeros((SAMPLES_T, NEURONS), dtype='float32')
+        target_array = np.empty((SAMPLES_T, NEURONS), dtype="float32")
+        target_array.fill(0.1)
         for t in range(SAMPLES_T):
-            target_array[t][int(array[t])] = 1
+            target_array[t][int(test_array[t])] = 0.9
+        print(target_array[9999])
         return target_array
 
     train_images, train_labels = read_train_data()
@@ -58,9 +61,11 @@ def read_to_dat():
     fp0 = np.memmap(train_images_dat, dtype='float64',
                     mode='w+', shape=(SAMPLES,INPUTS))
     fp0[:] = train_images[:]
+    del fp0
     fp1 = np.memmap(train_labels_dat, dtype='float64',
                     mode='w+', shape=(SAMPLES,NEURONS))
     fp1[:] = train_labels[:]
+    del fp1
 
     test_images, test_labels = read_test_data()
     test_labels = convert_targets_test(test_labels)
@@ -68,8 +73,10 @@ def read_to_dat():
     fp2 = np.memmap(test_images_dat, dtype='float64',
                     mode='w+', shape=(SAMPLES_T,INPUTS))
     fp2[:] = test_images[:]
+    del fp2
     fp3 = np.memmap(test_labels_dat, dtype='float64',
                     mode='w+', shape=(SAMPLES_T,NEURONS))
     fp3[:] = test_labels[:]
+    del fp3
 
 read_to_dat()

train.py
@@ -1,7 +1,21 @@
+from mlxtend.data import loadlocal_mnist
 import numpy as np
 import os
 import sys
 import warnings
+
+# t:	    ( SAMPLES , 10 )
+# x:	    ( SAMPLES , INPUTS+1 )
+# w_h:	( INPUTS+1 , NEURONS_H )
+# n_h:	( SAMPLES , NEURONS_H+1 )
+# w_o:	( NEURONS_H+1 , NEURONS )
+# n_o:	( SAMPLES , NEURONS )
+# o_delt:	( SAMPLES, NEURONS )
+# h_delt:	( SAMPLES , NEURONS_H+1 )
+# u_o:	( NEURONS_H+1 , NEURONS )
+# w_o:	( NEURONS_H+1 , NEURONS )
+# u_h:	( INPUTS+1 , NEURONS_H )
+# w_h:	( INPUTS+1 , NEURONS_H )
 
 class mlp:
     # Number of inputs
@@ -19,9 +33,9 @@ class mlp:
     EPOCHS = 5
     # Learning Rate
     # LR = float(sys.argv[1])
-    LR = .001
+    LR = .01
     # Momentum term
-    ALPHA = .9
+    ALPHA = 0
     # Array of epochs to store correct %
     CORRECT = []
     # Absolute path
@@ -40,7 +54,17 @@ class mlp:
         n_h = np.array(np.zeros((self.SAMPLES,self.NEURONS_H+1)))
         w_o = np.random.uniform(-.05,.05,size=(self.NEURONS_H+1,self.NEURONS))
         w_h = np.random.uniform(-.05,.05,size=(self.INPUTS+1,self.NEURONS_H))
+        print("w_o[0]\n",w_o[0])
+        print("w_h[0]\n",w_h[0])
         return n_o, n_h, w_o, w_h
+
+    def read_train_data(self):
+        train_images_raw = self.path + '/MNIST/rawdata/train-images.idx3-ubyte'
+        train_labels_raw = self.path + '/MNIST/rawdata/train-labels.idx1-ubyte'
+        x, y = loadlocal_mnist(
+                images_path = train_images_raw,
+                labels_path = train_labels_raw)
+        return x, y
 
     # Load training images and labels from data and add bias to images
     def load(self):
@@ -50,57 +74,76 @@ class mlp:
                     mode='r+', shape=(self.SAMPLES,self.NEURONS))
         x = np.c_[x, np.ones(self.SAMPLES)]
         return x, t
-
+    
+    # Sigmoid function
     def activate(self, n):
         try:
             n = 1 / (1 + np.exp(-n))
         except RuntimeWarning as r:
-            print('\nwarning! n==%s, r: %s' (n,r))
+        # except:
+            print('\nwarning! n==%s, r: %s' % (n,r))
 
     # Params: x==inputs, t==targets, n==neurons, w==weights, u==update,
     #         h==hidden, o==output
     def forward(self):
+
+        # Initialize prev update arrays
         u_h = np.zeros(np.shape(self.w_h))
         u_o = np.zeros(np.shape(self.w_o))
-        # print('\nt: %s x %s' % (self.t.shape[0], self.t.shape[1]))
-        # print('x: %s x %s' % (self.x.shape[0], self.x.shape[1]))
-        # print('w_h: %s x %s' % (self.w_h.shape[0], self.w_h.shape[1]))
-        # print('n_h: %s x %s' % (self.n_h.shape[0], self.n_h.shape[1]))
-        # print('w_o: %s x %s' % (self.w_o.shape[0], self.w_o.shape[1]))
-        # print('n_o: %s x %s' % (self.n_o.shape[0], self.n_o.shape[1]))
+
+        # Add number of hidden units and momentum term to our statistics array
         self.CORRECT.append(self.NEURONS_H)
         self.CORRECT.append(self.ALPHA)
+
+        print("\nforward()")
+        print("w_o[0]\n",self.w_o[0])
+        print("w_h[0]\n",self.w_h[0])
+
         for e in range(self.EPOCHS):
+
             confusion = []
-            print('Epoch: %s lr: %s n: %s N: %s' % (e+1, self.LR, self.NEURONS_H, self.SAMPLES))
+            print('\nEpoch: %s lr: %s n: %s N: %s' % (e+1, self.LR, self.NEURONS_H, self.SAMPLES))
+
+            # Get hidden units, activate, add bias, then 
             self.n_h = np.dot(self.x, self.w_h)
-            # print('n_h: %s x %s' % (self.n_h.shape[0], self.n_h.shape[1]))
-            self.n_h = np.c_[self.n_h,np.ones(self.SAMPLES)]
-            # print('n_h = np.dot(x,w_h): %s x %s' % (self.n_h.shape[0], self.n_h.shape[1]))
             self.activate(self.n_h)
-#            # print('a(n_h): %s x %s' % (n_h.shape[0], n_h.shape[1]))
+            self.n_h = np.c_[self.n_h,np.ones(self.SAMPLES)]
             self.n_o = np.dot(self.n_h, self.w_o)
-            # print('n_o = np.dot(n_h, w_o): %s x %s' % (self.n_o.shape[0], self.n_o.shape[1]))
-#            # print('n_o: %s x %s' % (n_o.shape[0], n_o.shape[1]))
             self.activate(self.n_o)
-#            # print('a(n_o): %s x %s' % (n_o.shape[0], n_o.shape[1]))
 
+            # Update confusion matrix
+            # for N in range(self.SAMPLES):
+            #     self.n_o[N] = np.where(self.n_o[N]>=np.amax(self.n_o[N]),1,0)
+            # temp_target = np.zeros((self.SAMPLES,self.NEURONS))
+            # for N in range(self.SAMPLES):
+            #     temp_target[N] = np.where(self.t[N]>=np.amax(self.t[N]),1,0)
+
+            # Get deltas
             o_deltas = self.n_o * (1-self.n_o) * (self.t-self.n_o)
-            # print('o_deltas: %s x %s' % (o_deltas.shape[0], o_deltas.shape[1]))
-
+            print("n_o[0]\n", self.n_o[0])
+            print("t[0]\n", self.t[0])
             h_deltas = self.n_h * (1-self.n_h) * np.dot(o_deltas,np.transpose(self.w_o))
-            # print('h_deltas: %s x %s' % (h_deltas.shape[0], h_deltas.shape[1]))
+            print("o_deltas[0]\n", o_deltas[0])
 
             # Hidden to output weight update
             u_o = self.LR * np.dot(np.transpose(self.n_h), o_deltas) + self.ALPHA * u_o
-            self.w_o += u_o
+            self.w_o -= u_o
+            print("w_o[0]\n",self.w_o[0])
+
             # Input to hidden weight update
             u_h = self.LR * np.dot(np.transpose(self.x), h_deltas[:,:-1]) + self.ALPHA * u_h
-            self.w_h += u_h
-#            # h_update = LR * (np.dot(inputs),h_deltas) + ALPHA * h_update
+            self.w_h -= u_h
+            print("w_h[0]\n",self.w_h[0])
+
+
+            # Update confusion matrix
+            for N in range(self.SAMPLES):
+                self.n_o[N] = np.where(self.n_o[N]>=np.amax(self.n_o[N]),1,0)
+            temp_target = np.zeros((self.SAMPLES,self.NEURONS))
+            for N in range(self.SAMPLES):
+                temp_target[N] = np.where(self.t[N]>=np.amax(self.t[N]),1,0)
+
             confusion = np.dot(np.transpose(self.t), self.n_o)
-            # print(self.t)
-            # print(self.CORRECT)
             accuracy = np.trace(confusion) / self.SAMPLES
             self.CORRECT.append(accuracy)
             print('  %:', round(accuracy*100, 4))
@@ -117,15 +160,6 @@ def main():
     run = mlp()
     run.forward()
     # run.back
-
-    # print('inputs: %s x %s' % (inputs.shape[0], inputs.shape[1]))
-    # print('targets: %s x %s' % (targets.shape[0], targets.shape[1]))
-    # print('oneurons: %s x %s' % (oneurons.shape[0], oneurons.shape[1]))
-    # print('hneurons: %s x %s' % (hneurons.shape[0], hneurons.shape[1]))
-    # print('oweights: %s x %s' % (oweights.shape[0], oweights.shape[1]))
-    # print('hweights: %s x %s' % (hweights.shape[0], hweights.shape[1]))
-    # print('oupdates: %s x %s' % (oupdates.shape[0], oupdates.shape[1]))
-    # print('hupdates: %s x %s' % (hupdates.shape[0], hupdates.shape[1]))
 
 if __name__ == '__main__':
     main()